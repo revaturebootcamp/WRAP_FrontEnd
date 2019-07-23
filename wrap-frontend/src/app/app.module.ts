7a6b1e25485ceaddeee42bf2ad85a6e8e12c3588
DannieJWright <Danniejw@gmail.com>
2019-07-23 17:13:38 -0500

    Merges conflicting declarations

--- wrap-frontend/src/app/app.module.ts
+++ wrap-frontend/src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RegisterComponent } from './components/register/register.component';
-=======
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
->>>>>>> 0d86230c79a0b3289896a72d826e146a54539b33
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    RegisterComponent
-=======
+    RegisterComponent,
     NavbarComponent,
     LoginComponent
->>>>>>> 0d86230c79a0b3289896a72d826e146a54539b33
   ],
   imports: [
     BrowserModule,
