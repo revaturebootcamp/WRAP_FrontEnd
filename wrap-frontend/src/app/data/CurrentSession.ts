21dee592ddc1fc1162cd183d8edfd704e655b027
DannieJWright <Danniejw@gmail.com>
2019-07-29 19:39:33 -0500

    modifies starting conditions

--- wrap-frontend/src/app/data/CurrentSession.ts
+++ wrap-frontend/src/app/data/CurrentSession.ts
@@ -1,12 +1,10 @@
 
 
 export class CurrentSession {
-    public static user : UserAccount = new UserAccount ();
+    public static user : UserAccount = null;
     public static currentRecipes : Recipe[] = [];
     public static favoriteRecipes : Recipe[] = [];
     public static historyRecipes : Recipe[] = [];
 
-    
-    constructor () {}
 
 }
