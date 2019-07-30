7e2a6acd40b78ad8ab43106f10285fd04cdade53
DannieJWright <Danniejw@gmail.com>
2019-07-29 19:37:01 -0500

    Adds class to store static data about current session

---
+++ wrap-frontend/src/app/data/CurrentSession.ts
@@ -0,0 +1,12 @@
+
+
+export class CurrentSession {
+    public static user : UserAccount = new UserAccount ();
+    public static currentRecipes : Recipe[] = [];
+    public static favoriteRecipes : Recipe[] = [];
+    public static historyRecipes : Recipe[] = [];
+
+    
+    constructor () {}
+
+}
