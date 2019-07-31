02bc63de8bf2ae2802c21553ebff0304fb0beb12
ben76013 <bigben37037@gmail.com>
2019-07-31 13:52:38 -0500

    Committing latest code

--- wrap-frontend/src/app/app-routing.module.ts
+++ wrap-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { registerLocaleData } from '@angular/common';
+// import { registerLocaleData } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { IngredientsComponent } from './components/ingredients/ingredients.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
