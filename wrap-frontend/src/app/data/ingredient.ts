e3a9c8896e65cb53223fb24bb91282ed1c6202d7
Paul Hernandez <p_h74@txstate.edu>
2019-08-01 14:41:12 -0500

    Ingredient page works

--- wrap-frontend/src/app/data/ingredient.ts
+++ wrap-frontend/src/app/data/ingredient.ts
@@ -6,4 +6,17 @@ export class Ingredient {
     aisle:string;
     amount:number;
     unit:string;
+
+    public clone () {
+        let temp : Ingredient = new Ingredient ();
+
+        temp.id = this.id;
+        temp.quantity = this.quantity;
+        temp.name = this.name;
+        temp.aisle = this.aisle;
+        temp.amount = this.amount;
+        temp.unit = this.unit;
+
+        return temp;
+    }
 }
